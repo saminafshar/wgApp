ca16c5e6a072b8d0b17c9ee9ed662e73b8a51df4
Samin Afshar <samin.afshar@haw-hamburg.de>
2020-02-11 23:28:10 +0100

    installed vuetify & vue router

---
+++ wg-ui/src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from "./components/HelloWorld";
+
+Vue.use(VueRouter);
+
+
+const routes = [
+    {path: '/home', component: HelloWorld}
+];
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+export default router;
+
