cd903147113c48145f09f0ad99e5c6da4704ee23
Samin Afshar <samin.afshar@haw-hamburg.de>
2020-02-12 00:40:29 +0100

    setup first graphql Query

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const {buildSchema} = require('graphql');
 
 const app = express();
-
+const schema = buildSchema(`
+    type Query {
+        hello: String
+        }
+    `);
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World');
-})
+app.use('/graphql', graphqlHttp({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
 
 app.listen(3000);
